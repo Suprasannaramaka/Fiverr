7211f0be37abffb0cc065a9e88133b49db21315b
Ramaka <ramakasuprasanna6@gmail.com>
2026-04-05 19:29:53 +0530

    init

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,22 @@ import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+       <div className='container'>
+        <div className='logo'>
+            <span className='text'>Fiverr</span>
+            <span className='dot'>.</span>
+        </div>
+        <div className='links'>
+            <span>Fiver Business</span>
+            <span>Explore</span>
+            <span>English</span>
+            <span>Sign In</span>
+            <span>Become A Seller</span>
+            <button type="button">Join</button>
+        </div>
+       </div>
+       </div>
   )
 }
 
